7b0d93a4db57c6e6b3c92d152b6a1a7d6c5c4fda
Andre Paraense <andre.paraense@mobwise.com.br>
2020-11-05 17:20:04 +0000

    iOS platform code initial implementation

--- ios/Classes/SwiftSurvicateFlutterSdkPlugin.swift
+++ ios/Classes/SwiftSurvicateFlutterSdkPlugin.swift
@@ -1,14 +1,92 @@
 import Flutter
 import UIKit
 
-public class SwiftSurvicateFlutterSdkPlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "survicate_flutter_sdk", binaryMessenger: registrar.messenger())
-    let instance = SwiftSurvicateFlutterSdkPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
+import Survicate
 
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
-  }
+public class SwiftSurvicateFlutterSdkPlugin: NSObject, FlutterPlugin, SurvicateDelegate {
+    
+    class FlutterChannel: NSObject {
+        static let shared: FlutterChannel = FlutterChannel()
+        var channel: FlutterMethodChannel?
+        private override init() {}
+    }
+    
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(name: "survicate_flutter_sdk", binaryMessenger: registrar.messenger())
+        let instance = SwiftSurvicateFlutterSdkPlugin()
+        registrar.addMethodCallDelegate(instance, channel: channel)
+        
+        FlutterChannel.shared.channel = channel
+        Survicate.shared.initialize()
+    }
+    
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        let arguments = call.arguments as? [String:String] ?? [:]
+        
+        if (call.method == "invokeEvent") {
+            let eventName = arguments["eventName"]  ?? ""
+            Survicate.shared.invokeEvent(name: eventName)
+            result(true)
+        } else if (call.method == "enterScreen") {
+            let screenName = arguments["screenName"]  ?? ""
+            Survicate.shared.enterScreen(value: screenName)
+            result(true)
+        } else if (call.method == "leaveScreen") {
+            let screenName = arguments["screenName"]  ?? ""
+            Survicate.shared.leaveScreen(value: screenName)
+            result(true)
+        } else if (call.method == "setUserTraits") {
+            
+            var traits = [UserTrait]()
+            
+            for (key, value) in arguments {
+                traits.append(UserTrait(withName: key, value: value))
+            }
+
+            Survicate.shared.setUserTraits(traits: traits)
+
+            result(true)
+        } else if (call.method == "reset") {
+            Survicate.shared.reset()
+            result(true)
+        } else if (call.method == "registerSurveyListeners") {
+            Survicate.shared.delegate = self
+            result(true)
+        } else if (call.method == "unregisterSurveyListeners") {
+            Survicate.shared.delegate = nil
+            result(true)
+        }else {
+            result(FlutterMethodNotImplemented)
+        }
+    }
+    
+    public func surveyDisplayed(surveyId: String) {
+        let arguments = ["surveyId": surveyId]
+        DispatchQueue.main.async {
+            FlutterChannel.shared.channel?.invokeMethod("onSurveyDisplayed", arguments: arguments)
+        }
+    }
+
+    public func questionAnswered(surveyId: String, questionId: Int, answer: SurvicateAnswer) {
+        let answerMap = ["type": answer.type as Any, "id": answer.id as Any , "ids": answer.ids as Any , "value" : answer.value as Any] as [String : Any]
+        
+        let arguments = ["surveyId": surveyId, "questionId": questionId, "answer": answerMap] as [String : Any]
+        DispatchQueue.main.async {
+            FlutterChannel.shared.channel?.invokeMethod("onQuestionAnswered", arguments: arguments)
+        }
+    }
+
+    public func surveyCompleted(surveyId: String) {
+        let arguments = ["surveyId": surveyId]
+        DispatchQueue.main.async {
+            FlutterChannel.shared.channel?.invokeMethod("onSurveyCompleted", arguments: arguments)
+        }
+    }
+
+    public func surveyClosed(surveyId: String) {
+        let arguments = ["surveyId": surveyId]
+        DispatchQueue.main.async {
+            FlutterChannel.shared.channel?.invokeMethod("onSurveyClosed", arguments: arguments)
+        }
+    }
 }
--- ios/survicate_flutter_sdk.podspec
+++ ios/survicate_flutter_sdk.podspec
@@ -4,8 +4,8 @@
 #
 Pod::Spec.new do |s|
   s.name             = 'survicate_flutter_sdk'
-  s.version          = '0.0.1'
-  s.summary          = 'A new flutter plugin project.'
+  s.version          = '1.0.0'
+  s.summary          = 'Survicate mobile survey SDK for Flutter.'
   s.description      = <<-DESC
 A new flutter plugin project.
                        DESC
@@ -15,7 +15,8 @@ A new flutter plugin project.
   s.source           = { :path => '.' }
   s.source_files = 'Classes/**/*'
   s.dependency 'Flutter'
-  s.platform = :ios, '8.0'
+  s.dependency 'Survicate', '1.4.2'
+  s.platform = :ios, '10.0'
 
   # Flutter.framework does not contain a i386 slice.
   s.pod_target_xcconfig = { 'DEFINES_MODULE' => 'YES', 'EXCLUDED_ARCHS[sdk=iphonesimulator*]' => 'i386' }
